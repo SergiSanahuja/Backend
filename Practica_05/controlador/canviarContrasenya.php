0d2a44432f5c08ec4d6369244c97c7eb53dc045c
Sergi Sanahuja <80123937+SergiSanahuja@users.noreply.github.com>
2023-11-06 18:44:09 +0100

    token fet

--- Practica_05/controlador/canviarContrasenya.php
+++ Practica_05/controlador/canviarContrasenya.php
@@ -14,17 +14,21 @@ if (isset($_GET['id'], $_GET['token'])){
 //Comprovem que les dades siguin correctes
 if(isset($_POST['password1'] , $_POST['password2'])){
     if($_POST['password1'] == $_POST['password2']){
-        $password = $_POST['password1'];
-        $password = password_hash($password, PASSWORD_BCRYPT);
-        modificarContrasenya($password,$_SESSION['id']);
-        
-        sleep(3);
-        header("Location: ../vista/login.vista.php?error=Contrasenya canviada correctament");
+        if (comprovarToken($_SESSION['token'])){   
+            $password = $_POST['password1'];
+            $password = password_hash($password, PASSWORD_BCRYPT);
+            modificarContrasenya($password,$_SESSION['id']);
+            
+            sleep(3);
+            header("Location: ../vista/login.vista.php?error=Contrasenya canviada correctament");
+        }else{
+            echo "<script>alert('El token no es correcte')</script>";
+        }
     }else{
         echo "Les contrasenyes no coincideixen";
     }
-}
 
+}
 
 if(isset($_GET['error'])){
     echo "<br>";                
